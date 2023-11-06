36eebb6004c55fa2256fd28b185bd5ff00fd8b74
Galuh80 <galuh.ibrahiim@gmail.com>
2023-11-06 12:01:40 +0700

    update: text login

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-admin.site.site_header = 'Eureka Xpert CCS'
-admin.site.site_title = 'Eureka Xpert CCS'
+admin.site.site_header = 'Eureka Xpert CMS'
+admin.site.site_title = 'Eureka Xpert CMS'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
