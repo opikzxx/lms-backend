bf5dd4d4410dbd9a5046fea70efe5e239b77e579
Galuh80 <galuh.ibrahiim@gmail.com>
2023-11-06 13:24:48 +0700

    update: deploy configuration

--- core/urls.py
+++ core/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_header = 'Eureka Xpert CMS'
 admin.site.site_title = 'Eureka Xpert CMS'
@@ -24,3 +27,5 @@ urlpatterns = [
     path('api/user/', include("authentication.urls")),
     path('api/courses/', include('course.urls_api')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
